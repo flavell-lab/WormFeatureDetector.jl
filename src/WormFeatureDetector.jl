25a15e71668687a9b7a40ec37017399007b5c184
goldenratio1618 <adamatanas@gmail.com>
2020-08-20 15:29:32 -0400

    Updated code to use Rotations.jl package for RotMatrix.

--- src/WormFeatureDetector.jl
+++ src/WormFeatureDetector.jl
@@ -1,7 +1,7 @@
 module WormFeatureDetector
 
 using WormCurveFinder, MHDIO, Statistics, Plots, ProgressMeter, Images, ImageTransformations, HDF5,
-        LinearAlgebra, CoordinateTransformations, StaticArrays, ImageDataIO, FlavellBase, Interpolations
+        LinearAlgebra, CoordinateTransformations, StaticArrays, ImageDataIO, FlavellBase, Interpolations, Rotations
 
 include("worm_feature_detector.jl")
 include("worm_curve_finder.jl")
