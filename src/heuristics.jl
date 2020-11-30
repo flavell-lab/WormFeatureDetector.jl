89a73609a62928053077a6badca0e87bce7effbf
Jungsoo Kim <urlicht@users.noreply.github.com>
2020-11-30 18:04:03 -0500

    Update heuristics.jl
     - elastix_difficulty_wormcurve! to work on parameter dictionaries
     - changing elastix_difficulty_wormcurve! to make styling consistent, reduce ambiguity

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -40,66 +40,78 @@ Requires that the data be filtered in some way (eg: total-variation filtering),
 and that the head position of the worm is known in each frame.
 
 # Arguments:
-- `rootpath::String`: working directory path; all other directory inputs are relative to this
-- `frame1::Integer`: first frame
-- `frame2::Integer`: second frame
-- `mhd_path::String`: path to MHD image files
-- `head_path::String`: path to a file containing positions of the worm's head at each frame.
-- `img_prefix::String`: image prefix not including the timestamp. It is assumed that each frame's filename
-    will be, eg, `img_prefix_t0123_ch2.mhd` for frame 123 with channel=2.
-- `channel::Integer`: channel being used.
-- `curves`: dictionary of worm curves found so far. The method will attempt to find the worm's curvature in this dictionary,
+- `curves::Dict`: dictionary of worm curves found so far. The method will attempt to find the worm's curvature in this dictionary,
     and will compute and add it to the dictionary if not found.
+- `img1::Array{<:AbstractFloat,3}`: image 1 array (volume)
+- `img2::Array{<:AbstractFloat,3}`: image 2 array (volume)
+- `t1::Int`: index of frame 1
+- `t2::Int`: index of frame 2
+- `head_pos::Dict`: head position dictionaru
 
 ## Other parameters (optional):
-- `figure_save_path::String`: Path to save figures of worm curvature. If left blank, figures will not be generated.
-    If supplied, the figure for frame 1 will be generated.
+- `path_fig::Union{Nothing,String}`: Path to save figures of worm curvature. If `nothing`, figures will not be generated.
 - `downscale::Integer`: log2(factor) by which to downscale the image before processing. Default 3 (ie: downscale by a factor of 8)
 - `num_points::Integer`: number of points (not including head) in generated curve. Default 9.
 - `headpt::Integer`: First position from head (in index of curves) to be aligned. Default 4.
 - `tailpt::Integer`: Second position from head (in index of curves) to be aligned. Default 7.
 """
-function elastix_difficulty_wormcurve!(curves::Dict, rootpath::String, frame1::Integer, frame2::Integer, mhd_path::String, head_path::String,
-        img_prefix::String, channel::Integer; figure_save_path::String="",
-        downscale::Integer=3, num_points::Integer=9, headpt::Integer=4, tailpt::Integer=7)
+function elastix_difficulty_wormcurve!(curves::Dict, img1::Array{<:AbstractFloat,3}, img2::Array{<:AbstractFloat,3},
+        t1::Int, t2::Int, head_pos::Dict; downscale::Int=3, num_points::Int=9, headpt::Int=4, tailpt::Int=7,
+        path_fig::Union{Nothing,String}=nothing)
 
+    img1 = maxprj(img1, dims=3)
+    img2 = maxprj(img2, dims=3)
 
-    img1 = Float64.(maxprj(read_mhd(rootpath, img_prefix, mhd_path, frame1, channel), dims=3))
-
-    img2 = Float64.(maxprj(read_mhd(rootpath, img_prefix, mhd_path, frame2, channel), dims=3))
-
-    head_pos = read_head_pos(joinpath(rootpath, head_path))
-
-    if frame1 in keys(curves)
-        x1_c, y1_c = curves[frame1]
+    if haskey(curves, t1)
+        x1_c, y1_c = curves[t1]
     else
-        x1_c, y1_c = find_curve(img1, downscale, head_pos[frame1]./2^downscale, num_points)
+        x1_c, y1_c = find_curve(img1, downscale, head_pos[t1]./2^downscale, num_points)
+        curves[t1] = (x1_c, y1_c)
+    end
+    
+    if haskey(curves, t2)
+        x2_c, y2_c = curves[t2]
+    else
+        x1_c, y1_c = find_curve(img2, downscale, head_pos[t2]./2^downscale, num_points)
+        curves[t2] = (x2_c, y2_c)
     end
 
-    if frame2 in keys(curves)
-        x2_c, y2_c = curves[frame2]
-    else
-        x2_c, y2_c = find_curve(img2, downscale, head_pos[frame2]./2^downscale, num_points)
-    end
-
-    if figure_save_path != ""
-        create_dir(joinpath(rootpath, figure_save_path))
-        if !(frame1 in keys(curves))
+    if !isnothing(path_dir_fig)
+        create_dir(path_dir_fig)
+        if !haskey(curves, t1)
             fig = heatmap(transpose(img1), fillcolor=:grays, aspect_ratio=1, flip=false, showaxis=false, legend=false)
             scatter!(fig, x1_c.-1, y1_c.-1, color="red");
             scatter!(fig, [x1_c[1].-1], [y1_c[1].-1], color="cyan", markersize=5);
-            savefig(fig, joinpath(rootpath, figure_save_path, "$(frame1).png"));
+            savefig(fig, joinpath(path_dir_fig, "$(t1).png"));
         end
-        if !(frame2 in keys(curves))
+        if !haskey(curves, t2)
             fig = heatmap(transpose(img2), fillcolor=:grays, aspect_ratio=1, flip=false, showaxis=false, legend=false)
             scatter!(fig, x2_c.-1, y2_c.-1, color="red");
             scatter!(fig, [x2_c[1].-1], [y2_c[1].-1], color="cyan", markersize=5);
-            savefig(fig, joinpath(rootpath, figure_save_path, "$(frame2).png"));
+            savefig(fig, joinpath(path_dir_fig, "$(t2).png"));
         end
     end
 
-    curves[frame1] = (x1_c, y1_c)
-    curves[frame2] = (x2_c, y2_c)
-
     return curve_distance(x1_c, y1_c, x2_c, y2_c, headpt=headpt, tailpt=tailpt)
+end
+
+# TODO: document
+function elastix_difficulty_wormcurve!(curves::Dict, param::Dict, param_path::Dict, t1::Int, t2::Int, ch::Int,
+        path_dir_mhd::String, f_basename::Function; path_fig::Union{Nothing,String}=nothing)
+    worm_curve_n_pts = param["worm_curve_n_pts"] 
+    worm_curve_tail_idx = param["worm_curve_tail_idx"]
+    worm_curve_head_idx = param["worm_curve_head_idx"]
+    worm_curve_downscale = param["worm_curve_downscale"]
+    
+    path_mhd_t1 = joinpath(path_dir_mhd, f_basename(t1, ch) * ".mhd")
+    path_mhd_t2 = joinpath(path_dir_mhd, f_basename(t2, ch) * ".mhd")
+
+    img1 = Float64.(read_img(MHD(path_mhd_t1)))
+    img2 = Float64.(read_img(MHD(path_mhd_t2)))
+    
+    head_pos = read_head_pos(param_path["path_head_pos"])
+
+    elastix_difficulty_wormcurve!(curves, img1, img2, t1, t2, head_pos,
+        downscale=worm_curve_downscale, num_points=worm_curve_n_pts, headpt=param["worm_curve_head_idx"],
+        tailpt=param["worm_curve_tail_idx"], path_fig=path_fig)
 end
