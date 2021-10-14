28efd277343b121f8cc1c91d37162461f3f280c6
goldenratio1618 <adamatanas@gmail.com>
2021-10-14 14:15:01 -0400

    Moved head detection to SegmentationTools

--- src/WormFeatureDetector.jl
+++ src/WormFeatureDetector.jl
@@ -1,6 +1,6 @@
 module WormFeatureDetector
 
-using WormCurveFinder, MHDIO, UNet2D, Statistics, Plots, ProgressMeter, Images, ImageTransformations, HDF5, ImageSegmentation,
+using WormCurveFinder, MHDIO, Statistics, Plots, ProgressMeter, Images, ImageTransformations, HDF5, ImageSegmentation,
         LinearAlgebra, CoordinateTransformations, StaticArrays, ImageDataIO, FlavellBase, Interpolations, Rotations
 
 include("worm_feature_detector.jl")
@@ -10,8 +10,6 @@ include("find_head.jl")
 
 export
     find_gut_granules,
-    find_head,
-    find_head_unet,
     find_hsn,
     find_nerve_ring,
     in_conv_hull,
